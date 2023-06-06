573766631fd05b71703f404d16f7b6536fbd8d1b
DevvSakib <devvsakib@gmail.com>
2023-06-06 11:17:37 +0600

    all file added

---
+++ src/components/common/DHButton.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DHButton = () => {
+  return (
+    <div>DHButton</div>
+  )
+}
---
+++ src/components/common/DHTitle.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const DHTitle = () => {
+  return (
+    <div>DHTitle</div>
+  )
+}
